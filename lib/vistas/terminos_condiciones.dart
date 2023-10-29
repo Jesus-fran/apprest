783aeb64ca66a21cd347bcf1649cf53a412f4607
Jesus-fran <jesusfran.dev@gmail.com>
2023-10-28 18:23:32 -0600

    se mejora el contenido de la interfaz de terminos y condiciones

--- lib/vistas/terminos_condiciones.dart
+++ lib/vistas/terminos_condiciones.dart
@@ -38,18 +38,15 @@ class _TerminosCondicionesState extends State<TerminosCondiciones> {
 }
 
 Widget _terminosCondiciones() {
-  String content =
-      'Digital solution se reserva el derecho de monitorear todos los comentarios y eliminar los que puedan considerarse inapropiados, ofensivos o que incumplan estos Términos y Condiciones. Garantizas y declaras que: Tienes derecho a publicar comentarios en nuestro sitio web y tienes todas las licencias y consentimientos necesarios para hacerlo; Los comentarios no invaden ningún derecho de propiedad intelectual, incluidos, entre otros, los derechos de autor, patentes o marcas comerciales de terceros; Los comentarios no contienen ningún material difamatorio, calumnioso, ofensivo, indecente o ilegal de otro modo, que sea una invasión de la privacidad. Los comentarios no se utilizarán para solicitar o promover negocios o actividades comerciales personalizadas o presentes o actividades ilegales. Por la presente, otorgas a Digital solution una licencia no exclusiva para usar, reproducir, editar y autorizar a otros a usar, reproducir y editar cualquiera de tus comentarios en todas y cada una de las formas, formatos, o medios. Hipervínculos a nuestro contenido: Las siguientes organizaciones pueden vincularse a nuestro sitio web sin aprobación previa por escrito: Agencias gubernamentales; Motores de búsqueda; Organizaciones de noticias; Los distribuidores de directorios en línea pueden vincularse a nuestro sitio web de la misma manera que hacen hipervínculos a los sitios web de otras empresas que figuran en la lista; y Empresas acreditadas en todo el sistema, excepto que soliciten organizaciones sin fines de lucro, centros comerciales de caridad y grupos de recaudación de fondos de caridad que no pueden hacer hipervínculos a nuestro sitio web. Estas organizaciones pueden enlazar a nuestra página de inicio, a publicaciones o a otra información del sitio siempre que el enlace: (a) no sea engañoso de ninguna manera; (b) no implique falsamente patrocinio, respaldo o aprobación de la parte vinculante y sus productos y/o servicios; y (c) encaja en el contexto del sitio de la parte vinculante. Podemos considerar y aprobar otras solicitudes de enlaces de los siguientes tipos de organizaciones';
-
-  return SingleChildScrollView(
+  return const SingleChildScrollView(
     child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: Column(
           children: [
-            const SizedBox(
+            SizedBox(
               height: 20,
             ),
-            const Row(
+            Row(
               children: [
                 Text(
                   '¿Qué es Perfect Connection?',
@@ -58,28 +55,201 @@ Widget _terminosCondiciones() {
                 ),
               ],
             ),
-            const SizedBox(
+            SizedBox(
               height: 20,
             ),
-            const Text(
-              'PerfectConection es una plataforma innovadora diseñada para potenciar y promover negocios locales, centrándose particularmente en el sector alimenticio (restaurantes, reto-bares, cocinas económicas, fondas, entre otros). Nuestra misión es brindar visibilidad a aquellos emprendedores que, debido a la falta de experiencia o recursos, encuentran desafiantes diseñar campañas publicitarias efectivas para sus establecimientos.',
+            Text(
+              'PerfectConection es una plataforma innovadora diseñada para potenciar y promover negocios, centrándose particularmente en el sector alimenticio (restaurantes, reto-bares, cocinas económicas, fondas, entre otros).',
               style: TextStyle(fontSize: 18),
               textAlign: TextAlign.justify,
             ),
-            const SizedBox(
+            SizedBox(
               height: 20,
             ),
-            const Text(
-                'Estos términos y condiciones describen las reglas y regulaciones para el uso del sitio web de Digital solution, ubicado en perfectconnection.com'),
-            const Text('Licencia:'),
-            const Text(
-                'A menos que se indique lo contrario, Digital solution y/o sus licenciantes poseen los derechos de propiedad intelectual de todo el material en Perfect Connection. Todos los derechos de propiedad intelectual son reservados. Puedes acceder desde Perfect Connection para tu uso personal sujeto a las restricciones establecidas en estos términos y condiciones.'),
-            const Text('No debes: '),
-            const Text(
-                'Copiar o volver a publicar material de Perfect Connection Vender, alquilar o sublicenciar material de Perfect Connection Reproducir, duplicar o copiar material de Perfect Connection Redistribuir contenido de Perfect Connection Este acuerdo comenzará el fecha presente.'),
-            const Text(
-                'Partes de esta aplicación móvil ofrecen a los usuarios la oportunidad de publicar e intercambiar opiniones e información en determinadas áreas. Digital solution no filtra, edita, publica ni revisa los comentarios antes de su presencia en el sitio web. Los comentarios no reflejan los puntos de vista ni las opiniones de Digital solution, sus agentes y/o afiliados. Los comentarios reflejan los puntos de vista y opiniones de la persona que publica. En la medida en que lo permitan las leyes aplicables, Digital solution no será responsable de los comentarios ni de ninguna responsabilidad, daños o gastos causados ​​o sufridos como resultado de cualquier uso o publicación o apariencia de comentarios en este sitio web.'),
-            Text(content),
+            Text(
+              'Términos y Condiciones de Uso de Perfect Connection',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Fecha de entrada en vigor: 28 de Octubre del 2023',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Por favor, lea detenidamente estos términos y condiciones de uso ("Términos y Condiciones") antes de utilizar la aplicación Perfect Connection.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '1. Aceptación de los Términos y Condiciones',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Al utilizar la Aplicación, usted acepta y se compromete a cumplir estos Términos y Condiciones. Si no está de acuerdo con estos Términos y Condiciones, no utilice la Aplicación.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '2. Registro',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Para acceder y utilizar ciertas funciones de la Aplicación, es posible que deba registrarse y crear una cuenta. Usted se compromete a proporcionar información precisa y actualizada durante el proceso de registro y a mantener sus datos de contacto actualizados.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Usted es responsable de mantener la confidencialidad de su nombre de usuario y contraseña y acepta no compartir esta información con terceros. Usted será el único responsable de todas las actividades que ocurran en su cuenta.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '3. Política de Privacidad',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'El uso de la Aplicación está sujeto a nuestra Política de Privacidad, que se encuentra en .... . Al utilizar la Aplicación, usted acepta las prácticas de privacidad descritas en la Política de Privacidad.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '4. Contenido de la Aplicación',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'La Aplicación permite a los restaurantes y usuarios cargar y compartir contenido, como fotos, reseñas y comentarios. Usted es el único responsable del contenido que carga y comparte en la Aplicación.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Usted se compromete a no cargar ni compartir contenido que sea difamatorio, obsceno, ilegal o que viole los derechos de terceros, incluyendo derechos de propiedad intelectual.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '5. Responsabilidad y Exención de Garantía',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'La Aplicación se proporciona "tal cual" y "según disponibilidad", sin garantías de ningún tipo. No garantizamos que la Aplicación sea ininterrumpida, segura o libre de errores.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'No somos responsables de los actos u omisiones de los usuarios de la Aplicación. Usted utiliza la Aplicación bajo su propio riesgo.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '6. Ley Aplicable y Jurisdicción',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Estos Términos y Condiciones se regirán e interpretarán de conformidad con las leyes de los Estados Unidos Mexicanos y cualquier disputa que surja o esté relacionada con estos Términos y Condiciones estará sujeta a la jurisdicción de los tribunales del estado de Chiapas, México.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  '7. Cambios en los Términos y Condiciones',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Nos reservamos el derecho de modificar estos Términos y Condiciones en cualquier momento. Cualquier cambio se notificará en la Aplicación. El uso continuo de la Aplicación después de dichos cambios constituirá su aceptación de los nuevos Términos y Condiciones.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 20,
+            ),
           ],
         )),
   );
