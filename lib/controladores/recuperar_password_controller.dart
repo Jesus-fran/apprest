109147a8af8612cc40b18e7f9e83696220fe4c6f
Jesus-fran <jesusfran.dev@gmail.com>
2023-11-05 15:44:08 -0600

    Se implementa la funcionalidad de recuperación de contraseña

--- lib/controladores/recuperar_password_controller.dart
+++ lib/controladores/recuperar_password_controller.dart
@@ -3,19 +3,16 @@ import 'dart:convert';
 import 'package:baseapp/modelos/autenticacion_model.dart';
 import 'package:http/http.dart' as http;
 
-String url = 'http://5ba9-201-153-54-81.ngrok-free.app/api/recovery-pass';
+String url = 'http://5ba9-201-153-54-81.ngrok-free.app/api/recovery-password';
 
-Future<bool> recuperarPassword(
-    String email) async {
+Future<AuthModelo> recuperarPassword(String email) async {
   await Future.delayed(const Duration(seconds: 3));
-  //final response = await http.post(Uri.parse(url),
-      //headers: {'Accept': 'application/json'},
-      //body: {'email': email});
-  //String body = utf8.decode(response.bodyBytes);
-  //final jsonData = authModeloFromJson(body);
-  //jsonData.statusCode = response.statusCode;
-  //print(body);
-  //print(response.statusCode);
-  //return jsonData;
-  return true;
+  final response = await http.post(Uri.parse(url),
+      headers: {'Accept': 'application/json'}, body: {'email': email});
+  String body = utf8.decode(response.bodyBytes);
+  final jsonData = authModeloFromJson(body);
+  jsonData.statusCode = response.statusCode;
+  print(body);
+  print(response.statusCode);
+  return jsonData;
 }
--- lib/vistas/enviando_email.dart
+++ lib/vistas/enviando_email.dart
@@ -1,4 +1,5 @@
 import 'package:baseapp/controladores/recuperar_password_controller.dart';
+import 'package:baseapp/modelos/autenticacion_model.dart';
 import 'package:flutter/material.dart';
 
 class EnviandoEmail extends StatefulWidget {
@@ -15,11 +16,26 @@ class _EnviandoEmailState extends State<EnviandoEmail> {
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: Center(
-          child: FutureBuilder<bool>(
+          child: FutureBuilder<AuthModelo>(
               future: recuperarPassword(widget.email),
-              builder: (context, AsyncSnapshot<bool> snapshot) {
+              builder: (context, AsyncSnapshot<AuthModelo> snapshot) {
                 if (snapshot.hasData) {
-                  return succesMessage(context);
+                  if (snapshot.data!.statusCode == 200 &&
+                      snapshot.data!.status) {
+                    //Si envia el email correctamente
+                    return succesMessage(context, snapshot.data!.message);
+                  } else if (snapshot.data!.statusCode == 200 &&
+                      !snapshot.data!.status) {
+                    //Si hubo un fallo al enviar el email
+                    return errorMessage(context, snapshot.data!.message);
+                  } else if (snapshot.data!.statusCode == 422) {
+                    //Si hubo un error de validación
+                    return validationMessage(snapshot.data!.message, context);
+                  } else {
+                    //Si hay un error desconocido
+                    return errorMessage(context,
+                        'Hubo un error al intentar enviar el correo de recuperación.');
+                  }
                 } else {
                   return enviandoMessage(context);
                 }
@@ -29,17 +45,17 @@ class _EnviandoEmailState extends State<EnviandoEmail> {
     );
   }
 
-  Widget succesMessage(BuildContext context) {
+  Widget succesMessage(BuildContext context, message) {
     void showAutoSnackBar(BuildContext context) {
       // Muestra si hay un error de validación
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-        SnackBar snack_1 = const SnackBar(
+        SnackBar snack_1 = SnackBar(
           content: Text(
-            'Correo de recuperación enviado con éxito. Revisa tu correo',
-            style: TextStyle(color: Colors.greenAccent, fontSize: 18),
+            message,
+            style: const TextStyle(color: Colors.greenAccent, fontSize: 18),
             textAlign: TextAlign.center,
           ),
-          duration: Duration(seconds: 25),
+          duration: const Duration(seconds: 25),
         );
         ScaffoldMessenger.of(context).showSnackBar(snack_1);
       });
@@ -57,19 +73,75 @@ class _EnviandoEmailState extends State<EnviandoEmail> {
     return const Column(
       children: [
         SizedBox(height: 200),
-        Text('Enviando correo de recuperación..',
-            style: TextStyle(fontSize: 30), textAlign: TextAlign.center,),
+        Text(
+          'Enviando correo de recuperación..',
+          style: TextStyle(
+              fontSize: 30,
+              color: Color.fromRGBO(47, 79, 79, 1),
+              fontWeight: FontWeight.w500),
+          textAlign: TextAlign.center,
+        ),
         SizedBox(height: 60),
         LinearProgressIndicator(
-          color: Colors.amberAccent,
+          color: Color.fromRGBO(255, 92, 27, 0.692),
         ),
         SizedBox(height: 60),
         Text(
           'Espere un momento, por favor.',
-          style: TextStyle(fontSize: 25),
+          style: TextStyle(fontSize: 25, color: Color.fromRGBO(47, 79, 79, 1), fontWeight: FontWeight.w400),
           textAlign: TextAlign.center,
         ),
       ],
     );
+  }
+
+  Widget validationMessage(String message, context) {
+    void showAutoSnackBar(BuildContext context) {
+      // Muestra si hay un error de validación
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        SnackBar snack_1 = SnackBar(
+          content: Text(
+            message,
+            style: const TextStyle(color: Colors.redAccent, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          duration: const Duration(seconds: 15),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snack_1);
+      });
+    }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+    });
+
+    showAutoSnackBar(context);
+
+    return Container();
+  }
+
+  Widget errorMessage(context, message) {
+    void showAutoSnackBar(BuildContext context) {
+      // Muestra si hay un error de validación
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        SnackBar snack_1 = SnackBar(
+          content: Text(
+            message,
+            style: const TextStyle(color: Colors.redAccent, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          duration: const Duration(seconds: 15),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snack_1);
+      });
+    }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+    });
+
+    showAutoSnackBar(context);
+
+    return Container();
   }
 }
